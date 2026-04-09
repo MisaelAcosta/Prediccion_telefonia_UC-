c515265b379945aa276e6a066bc3b1097f2bd691
Misael Acosta <acostamisael166@gmail.com>
2026-04-09 17:56:53 -0400

    primer commit

---
+++ src/preprocessing.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def cargar_datos(ruta):
+    df = pd.read_csv(ruta)
+    return df
+
+def limpiar_datos(df):
+    df["calldate"] = pd.to_datetime(df["calldate"])
+    df = df[df["billsec"] > 0]
+    return df
